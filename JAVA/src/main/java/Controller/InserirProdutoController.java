89c8020fb5b22ce89022691197174d82358394a9
gio-kytes <47891542+gio-kytes@users.noreply.github.com>
2019-04-26 18:43:41 -0300

    Add files via upload

---
+++ JAVA/src/main/java/Controller/InserirProdutoController.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import InterDao.ProdutoDaoInte;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import DAO.ProdutoDAO;
+import Model.Produto;
+
+/**
+ *
+ * @author IDCOM
+ */
+public class InserirProdutoController extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    private ProdutoDaoInte dao = (ProdutoDaoInte) ProdutoDAO.getInstance();
+
+    public InserirProdutoController() {
+        // Do Nothing
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        int id = Integer.parseInt(request.getParameter("IDPRODUTO"));
+        String nome = request.getParameter("NOME");
+        int quantidade = Integer.parseInt(request.getParameter("QUANTIDADE"));
+        float valor = Float.parseFloat(request.getParameter("VALOR"));
+        String descricao = request.getParameter("DESCRICAO");
+        boolean habilitado = Boolean.parseBoolean(request.getParameter("HABILITDAO"));
+
+        Produto produto = new Produto(nome, quantidade, valor, descricao, habilitado);
+
+        if (id == 0) {
+            dao.Inserir(produto);
+        } else {
+            int idP = id;
+            produto.setIdProduto(id);
+            dao.Atualizar(produto);
+        }
+
+        response.sendRedirect(request.getContextPath() + "/");
+    }
+
+}
